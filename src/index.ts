8c7242f2e9533e5579143359b180f1184a357729
Rachael Dawn <me@rachaeldawn.ca>
2022-08-15 13:13:01 -0600

    Demo for fix

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
-import { createEvent } from 'effector';
+import * as E from 'effector';
+import * as EV from 'effector-vue';
+import * as ER from 'effector-react';
+import * as ES from 'effector-solid';
+import * as forest from 'forest';
 
-export const event = createEvent();
+E; EV; ER; ES; forest;
